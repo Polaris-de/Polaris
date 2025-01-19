b3763051306beb303ff9e5f50db12cf6e0f8b896
Polaris-de <ax212c@gmail.com>
2025-01-19 10:49:41 +0200

    Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 sudo apt install kwin-x11 --no-install-recommends
 sudo apt install plank
 sudo apt install nitrogen
+sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y && sudo apt update && sudo apt install ulauncher
 sudo mv /home/user/Downloads/Polaris/Polaris.sh /home/user
 sudo mv /home/user/Downloads/Polaris/polaris.desktop /usr/share/xsessions
+chmod +x /home/user/polaris.sh
