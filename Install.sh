4d40d7eb5ea6abe181b089d484d8ba1e45590aa4
Polaris-de <ax212c@gmail.com>
2025-01-19 10:43:55 +0200

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,5 @@
+sudo apt install kwin-x11 --no-install-recommends
+sudo apt install plank
+sudo apt install nitrogen
+sudo mv /home/user/Downloads/Polaris/Polaris.sh /home/user
+sudo mv /home/user/Downloads/Polaris/polaris.desktop /usr/share/xsessions
---
+++ Polaris.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+kwin-x11 &
+nitrogen &
+plank
