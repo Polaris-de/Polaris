0ab84a84d1f6be73e6c2926c109e36bbbeb5d133
Polaris-de <ax212c@gmail.com>
2025-01-19 19:54:12 +0200

    Install.sh

--- Install.sh
+++ Install.sh
@@ -3,6 +3,10 @@ sudo apt install kwin-x11 --no-install-recommends
 sudo apt install plank
 sudo apt install nitrogen
 sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y && sudo apt update && sudo apt install ulauncher
+unzip /home/user/Downloads/Marwaita-OSX-main.zip
+chmod +x /home/user/polaris.sh
+mkdir -p ~/.config/gtk-3.0
+echo -e "[Settings]\ngtk-icon-theme-name=<icon_theme_name>" > ~/.config/gtk-3.0/settings.ini
+gtk-update-icon-cache /usr/share/icons/<icon_theme_name>
 sudo mv /home/user/Downloads/Polaris/Polaris.sh /home/user
 sudo mv /home/user/Downloads/Polaris/polaris.desktop /usr/share/xsessions
-chmod +x /home/user/polaris.sh
